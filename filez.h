f9173846be10da72433e6d57f73d32e031a21ba7
MKGirlism <yami@mkgirlism.jp>
2017-06-09 08:10:10 +0900

    I'll use "pragma" from now on, instead of "ifndef".

--- filez.h
+++ filez.h
@@ -1,15 +1,8 @@
-#ifndef __FILEZ_H__
-#define __FILEZ_H__
+#pragma once
 
 #include "getall.h"
-
-G_BEGIN_DECLS
 
 void open_func (GtkWidget *widget, gpointer window);
 void save_func (GtkWidget *widget, gpointer window);
 
 int isload;
-
-G_END_DECLS
-
-#endif
